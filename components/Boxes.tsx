574f99a34baac875df94d965a593dcaf43a60ddb
Ivylegend <victoryicha2@gmail.com>
2025-02-21 14:04:10 +0100

    mobile version

--- components/Boxes.tsx
+++ components/Boxes.tsx
@@ -8,7 +8,7 @@ declare type BoxesProps = {
 
 const Boxes = ({ title, body }: BoxesProps) => {
   return (
-    <div className="bg-white shadow-lg w-full md:w-[400px] p-5 rounded-md flex flex-col gap-3">
+    <div className="bg-white shadow-2xl w-full md:w-[400px] p-5 rounded-md flex flex-col gap-3">
       <LucideScanEye />
       <h2 className="font-semibold text-xl">{title}</h2>
       <p>{body}</p>
